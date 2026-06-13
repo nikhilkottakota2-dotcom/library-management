50abaf04efa996a64125079a9c7e27490617fd61
nikhilkottakota2-dotcom <nikhilkottakota2@gmail.com>
2026-06-13 17:15:24 +0530

    Refactor Library class and add Student management

--- Library.java
+++ Library.java
@@ -1,91 +1,56 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
-class Book {
+class Student_details{
+    int number;
     String name;
-    int count;
-
-    Book(String name, int count) {
-        this.name = name;
-        this.count = count;
-    }
-
-    @Override
-    public String toString() {
-        return name + " (Copies: " + count + ")";
-    }
+    String mobileNumber;
 }
 
-class Books {
-    ArrayList<Book> horrorBooks = new ArrayList<>();
-    ArrayList<Book> comedyBooks = new ArrayList<>();
-    ArrayList<Book> loveBooks = new ArrayList<>();
+public class Student{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
 
-    public void addBook(String type, String name, int count) {
-        Book book = new Book(name, count);
-        switch (type.toLowerCase()) {
-            case "horror":
-                horrorBooks.add(book);
-                System.out.println("Added Horror book: " + book);
-                break;
-            case "comedy":
-                comedyBooks.add(book);
-                System.out.println("Added Comedy book: " + book);
-                break;
-            case "love":
-                loveBooks.add(book);
-                System.out.println("Added Love book: " + book);
-                break;
-            default:
-                System.out.println("Invalid book type!");
-        }
-    }
-
-    public void showBooks() {
-        System.out.println("\n--- Library Books ---");
-        System.out.println("Horror: " + horrorBooks);
-        System.out.println("Comedy: " + comedyBooks);
-        System.out.println("Love: " + loveBooks);
-    }
-}
-
-public class Library {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Books library = new Books();
-        int choice;
-
-        do {
-            System.out.println("\nLibrary Menu:");
-            System.out.println("1. Add Book");
-            System.out.println("2. Show All Books");
-            System.out.println("3. Exit");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
-            scanner.nextLine(); // consume newline
+        while (choice != 2) {
+            System.out.println("Enter the function you want to:");
+            System.out.println("1. Add Student");
+            System.out.println("2. Exit");
+            choice = sc.nextInt();
+            sc.nextLine(); // consume newline
 
             switch (choice) {
                 case 1:
-                    System.out.print("Enter type of book (Horror/Comedy/Love): ");
-                    String type = scanner.nextLine();
-                    System.out.print("Enter book name: ");
-                    String name = scanner.nextLine();
-                    System.out.print("Enter the number of copies: ");
-                    int count = scanner.nextInt();
-                    scanner.nextLine(); // consume newline
-                    library.addBook(type, name, count);
-                    break;
-                case 2:
-                    library.showBooks();
-                    break;
-                case 3:
-                    System.out.println("Exiting Library...");
-                    break;
-                default:
-                    System.out.println("Invalid choice!");
-            }
-        } while (choice != 3);
+                    System.out.print("Enter how many students you want to add: ");
+                    int n = sc.nextInt();
+                    sc.nextLine(); // consume newline
+                    Student_details[] students = new Student_details[n];
 
-        scanner.close();
+                    for (int i = 0; i < students.length; i++) {
+                        students[i] = new Student_details();
+                        System.out.println("------Enter Student " + (i + 1) + " details------");
+
+                        System.out.print("Enter name of the student: ");
+                        students[i].name = sc.nextLine();
+
+                        System.out.print("Enter the roll number of the student: ");
+                        students[i].number = sc.nextInt();
+                        sc.nextLine(); // consume newline
+
+                        System.out.print("Enter the mobile number of the student: ");
+                        students[i].mobileNumber = sc.nextLine();
+                        
+                        System.out.println("------Student added succesfully------");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Exiting program...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
+        sc.close();
     }
 }
